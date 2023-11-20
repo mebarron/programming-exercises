2cfe7ef8a419eca370c517ce7501ae379dd6373b
Martin Barron <mebarron@me.com>
2023-11-19 19:11:21 -0500

    go fmt

--- arrays/diagonal_diff.go
+++ arrays/diagonal_diff.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"fmt"
@@ -6,48 +6,48 @@ import (
 )
 
 func diagonalDifference(arr [][]int32) int32 {
-    diag_sum1 := 0 
-    diag_sum2 := 0
-    
-    column := 0
-    
-    absolute_sum := 0
-    
-    for _, x := range arr {
-        diag_sum1 += int(x[column])
-        column++
-    }
-    
-    column = len(arr) - 1
-    
-    for _, x := range arr { 
-        diag_sum2 += int(x[column])
-        column-- 
-    }
-    
-    absolute_sum = diag_sum1 - diag_sum2
-    
-    if absolute_sum < 0 {
+	diag_sum1 := 0
+	diag_sum2 := 0
+
+	column := 0
+
+	absolute_sum := 0
+
+	for _, x := range arr {
+		diag_sum1 += int(x[column])
+		column++
+	}
+
+	column = len(arr) - 1
+
+	for _, x := range arr {
+		diag_sum2 += int(x[column])
+		column--
+	}
+
+	absolute_sum = diag_sum1 - diag_sum2
+
+	if absolute_sum < 0 {
 		absolute_sum -= absolute_sum + absolute_sum
 	}
-    
-    return int32(absolute_sum)
+
+	return int32(absolute_sum)
 }
 
 func main() {
-	var arr [][]int32 
+	var arr [][]int32
 
-	for i:=0;i<9;i++ {
+	for i := 0; i < 9; i++ {
 		var insert []int32
-		for i:=0;i<9;i++ {
+		for i := 0; i < 9; i++ {
 			r := rand.Intn(8)
 			if r >= 7 {
 				r = -1
 			}
-	
+
 			insert = append(insert, int32(r))
 		}
-		arr = append(arr, insert) 
+		arr = append(arr, insert)
 	}
 
 	for _, row := range arr {
